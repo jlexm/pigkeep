f3474ea822acce7f486038c9b0c34eb18e905885
mathew <90034902+mathewwww@users.noreply.github.com>
2024-09-06 15:37:12 +0800

    Tried debugging but failed

--- client_app/pig_keep/lib/Components/DataTable_PigPen.dart
+++ client_app/pig_keep/lib/Components/DataTable_PigPen.dart
@@ -1,10 +1,197 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
+// import 'package:pig_keep/Components/PigPen_PenNumber.dart';
+
+// class MyDataTable_Pigpen extends StatefulWidget {
+//   final void Function(Map<String, String>) onRowSelected;
+
+//   const MyDataTable_Pigpen({Key? key, required this.onRowSelected}) : super(key: key);
+
+//   @override
+//   State<MyDataTable_Pigpen> createState() => _MyDataTable_PigpenState();
+// }
+
+// class _MyDataTable_PigpenState extends State<MyDataTable_Pigpen> {
+//   final List<Map<String, dynamic>> _data = [
+//     {
+//       'number': 'P-01',
+//       'type': 'Nursery',
+//       'pig count': '3',
+//       'max pigs': '5',
+//       'status': 'alive',
+//       'pigs': ['001', '002', '003'],
+//     },
+//     {
+//       'number': 'P-02',
+//       'type': 'Stall',
+//       'pig count': '5',
+//       'max pigs': '5',
+//       'status': 'sold',
+//       'pigs': ['004', '005', '006', '007', '008'],
+//     },
+//     {
+//       'number': 'P-03',
+//       'type': 'Stall',
+//       'pig count': '6',
+//       'max pigs': '6',
+//       'status': 'deceased',
+//       'pigs': ['009', '010', '011', '012', '013', '014'],
+//     },
+//     {
+//       'number': 'P-04',
+//       'type': 'Farrowing',
+//       'pig count': '1',
+//       'max pigs': '1',
+//       'status': 'alive',
+//       'pigs': ['015'],
+//     },
+//   ];
+
+//   void _handleRowSelection(Map<String, dynamic> row) {
+//     Navigator.of(context).push(
+//       MaterialPageRoute(
+//         builder: (context) => PigPenPenNumber(
+//           number: row['number']!,
+//           type: row['type']!,
+//           pigCount: row['pig count']!,
+//           maxPigs: row['max pigs']!,
+//           pigNumbers: List<String>.from(row['pigs']),
+//         ),
+//       ),
+//     );
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: Center(
+//         child: SingleChildScrollView(
+//           scrollDirection: Axis.horizontal,
+//           child: DataTable(
+//             showCheckboxColumn: false,
+//             columnSpacing: 30.w,
+//             dividerThickness: 0,
+//             columns: [
+//               DataColumn(
+//                 headingRowAlignment: MainAxisAlignment.center,
+//                 label: Center(
+//                   child: Text(
+//                     'Number',
+//                     textAlign: TextAlign.center,
+//                     style: TextStyle(
+//                       fontSize: 12.sp,
+//                       fontWeight: FontWeight.w500,
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//               DataColumn(
+//                 headingRowAlignment: MainAxisAlignment.center,
+//                 label: Center(
+//                   child: Text(
+//                     'Type',
+//                     textAlign: TextAlign.center,
+//                     style: TextStyle(
+//                       fontSize: 12.sp,
+//                       fontWeight: FontWeight.w500,
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//               DataColumn(
+//                 headingRowAlignment: MainAxisAlignment.center,
+//                 label: Center(
+//                   child: Text(
+//                     'Pig Count',
+//                     textAlign: TextAlign.center,
+//                     style: TextStyle(
+//                       fontSize: 12.sp,
+//                       fontWeight: FontWeight.w500,
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//               DataColumn(
+//                 headingRowAlignment: MainAxisAlignment.center,
+//                 label: Center(
+//                   child: Text(
+//                     'Max Pigs',
+//                     textAlign: TextAlign.center,
+//                     style: TextStyle(
+//                       fontSize: 12.sp,
+//                       fontWeight: FontWeight.w500,
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ],
+//             rows: _data.map((row) {
+//               return DataRow(
+//                 onSelectChanged: (_) => _handleRowSelection(row),
+//                 cells: [
+//                   DataCell(
+//                     Center(
+//                       child: Text(
+//                         row['number']!,
+//                         style: TextStyle(
+//                           fontWeight: FontWeight.w400,
+//                           fontSize: 11.5.sp,
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                   DataCell(
+//                     Center(
+//                       child: Text(
+//                         row['type']!,
+//                         style: TextStyle(
+//                           fontWeight: FontWeight.w400,
+//                           fontSize: 11.5.sp,
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                   DataCell(
+//                     Center(
+//                       child: Text(
+//                         row['pig count']!,
+//                         style: TextStyle(
+//                           fontWeight: FontWeight.w400,
+//                           fontSize: 11.5.sp,
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                   DataCell(
+//                     Center(
+//                       child: Text(
+//                         row['max pigs']!,
+//                         style: TextStyle(
+//                           fontWeight: FontWeight.w400,
+//                           fontSize: 11.5.sp,
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                 ],
+//               );
+//             }).toList(),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
+
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:pig_keep/Components/PigPen_PenNumber.dart';
-import 'pig_pen_pen_number.dart';
 
 class MyDataTable_Pigpen extends StatefulWidget {
-  final void Function(Map<String, String>) onRowSelected;
+  final void Function(Map<String, dynamic>) onRowSelected;
 
   const MyDataTable_Pigpen({Key? key, required this.onRowSelected}) : super(key: key);
 
@@ -49,17 +236,7 @@ class _MyDataTable_PigpenState extends State<MyDataTable_Pigpen> {
   ];
 
   void _handleRowSelection(Map<String, dynamic> row) {
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (context) => PigPenPenNumber(
-          number: row['number']!,
-          type: row['type']!,
-          pigCount: row['pig count']!,
-          maxPigs: row['max pigs']!,
-          pigNumbers: List<String>.from(row['pigs']),
-        ),
-      ),
-    );
+    widget.onRowSelected(row);
   }
 
   @override
@@ -183,4 +360,3 @@ class _MyDataTable_PigpenState extends State<MyDataTable_Pigpen> {
     );
   }
 }
-
--- client_app/pig_keep/lib/Screens/Records.dart
+++ client_app/pig_keep/lib/Screens/Records.dart
@@ -1,3 +1,145 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
+// import 'package:pig_keep/Components/BottomNav.dart';
+// import 'package:pig_keep/Components/Carousel_PigCount.dart';
+// import 'package:pig_keep/Components/Chip.dart';
+// import 'package:pig_keep/Components/DataTable_PigList.dart';
+// import 'package:pig_keep/Components/FarmName.dart';
+// import 'package:pig_keep/Components/FeedInventory.dart';
+// import 'package:pig_keep/Components/MedicalRecords.dart';
+// import 'package:pig_keep/Components/PigList.dart';
+// import 'package:pig_keep/Components/PigPen.dart';
+// import 'package:pig_keep/Components/PigPen_PenNumber.dart';
+// import 'package:pig_keep/Components/QRCodeStatus.dart';
+// import 'package:pig_keep/Components/SearchBar_PigList.dart';
+// import 'package:pig_keep/Constants/color.constants.dart';
+// import 'package:pig_keep/Components/Hamburger.dart';
+
+// class Records extends StatefulWidget {
+//   const Records({super.key});
+
+//   @override
+//   State<Records> createState() => _RecordsState();
+// }
+
+// class _RecordsState extends State<Records> {
+//   int? _selectedChoiceIndex = 0;
+//   Map<String, String>? _selectedRowData;
+//   bool _showPigPenPenNumber = false;
+
+//   void _onChipSelected(int? index) {
+//     setState(() {
+//       _selectedChoiceIndex = index;
+//       _selectedRowData = null;
+//       _showPigPenPenNumber = false; // Reset to show PigPen when switching chips
+//     });
+//   }
+
+//   void _showQRCodeStatus(Map<String, String> rowData) {
+//     setState(() {
+//       _selectedChoiceIndex = null;
+//       _selectedRowData = rowData;
+//     });
+//   }
+
+//   void _showPigPenPenNumberScreen(Map<String, String> rowData) async {
+//     final result = await Navigator.push(
+//       context,
+//       MaterialPageRoute(
+//         builder: (context) => PigPenPenNumber(
+//           number: rowData['number']!,
+//           type: rowData['type']!,
+//           pigCount: rowData['pig count']!,
+//           maxPigs: rowData['max pigs']!,
+//           pigNumbers: []
+//         ),
+//       ),
+//     );
+
+//     if (result == 3) {
+//       setState(() {
+//         _selectedChoiceIndex = 3; // Set to PigPen chip
+//       });
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: appSecondary,
+//       drawer: Hamburger(),
+//       body: SafeArea(
+//         child: Column(
+//           children: [
+//             Expanded(
+//               child: SingleChildScrollView(
+//                 scrollDirection: Axis.vertical,
+//                 child: Column(
+//                   children: [
+//                     Row(
+//                       children: [
+//                         FarmName(),
+//                       ],
+//                     ),
+//                     SizedBox(height: 10.h),
+//                     Container(
+//                       padding: EdgeInsets.symmetric(horizontal: 20.w),
+//                       child: Text(
+//                         'Update your pig farm records to keep you on \ntrack with your pig farm management experience.',
+//                         style: TextStyle(
+//                           fontSize: 12.sp,
+//                           fontWeight: FontWeight.w400,
+//                         ),
+//                       ),
+//                     ),
+//                     SizedBox(
+//                       height: 6.h,
+//                     ),
+//                     Container(
+//                       padding: EdgeInsets.symmetric(horizontal: 15.w),
+//                       child: Chips(onChipSelected: _onChipSelected),
+//                     ),
+//                     SizedBox(
+//                       height: 10.h,
+//                     ),
+//                     Container(
+//                       //Container containing Screens
+//                       child: Column(
+//                         children: [
+//                           if (_selectedRowData != null && _showPigPenPenNumber)
+//                             PigPenPenNumber(
+//                               number: _selectedRowData!['number']!,
+//                               type: _selectedRowData!['type']!,
+//                               pigCount: _selectedRowData!['pig count']!,
+//                               maxPigs: _selectedRowData!['max pigs']!,
+//                               pigNumbers: [],
+//                             )
+//                           else if (_selectedRowData != null)
+//                             QRCodeStatus(pigData: _selectedRowData!)
+//                           else ...[
+//                             if (_selectedChoiceIndex == 0)
+//                               PigList(onRowSelected: _showQRCodeStatus),
+//                             if (_selectedChoiceIndex == 1) FeedInventory(),
+//                             if (_selectedChoiceIndex == 2) MedicalRecords(),
+//                             if (_selectedChoiceIndex == 3)
+//                               PigPen(onRowSelected: _showPigPenPenNumberScreen),
+//                           ],
+//                         ],
+//                       ),
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:pig_keep/Components/BottomNav.dart';
@@ -25,13 +167,47 @@ class Records extends StatefulWidget {
 class _RecordsState extends State<Records> {
   int? _selectedChoiceIndex = 0;
   Map<String, String>? _selectedRowData;
-  bool _showPigPenPenNumber = false;
+  Widget? _currentView;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentView = _buildInitialView();
+  }
+
+  Widget _buildInitialView() {
+    if (_selectedRowData != null) {
+      if (_selectedChoiceIndex == 3) {
+        return PigPenPenNumber(
+          number: _selectedRowData!['number']!,
+          type: _selectedRowData!['type']!,
+          pigCount: _selectedRowData!['pig count']!,
+          maxPigs: _selectedRowData!['max pigs']!,
+          pigNumbers: [], // Fetch and pass actual pig numbers if needed
+        );
+      }
+      return QRCodeStatus(pigData: _selectedRowData!);
+    } else {
+      switch (_selectedChoiceIndex) {
+        case 0:
+          return PigList(onRowSelected: _showQRCodeStatus);
+        case 1:
+          return FeedInventory();
+        case 2:
+          return MedicalRecords();
+        case 3:
+          return PigPen(onRowSelected: _showPigPenPenNumberScreen);
+        default:
+          return Container(); // Default view
+      }
+    }
+  }
 
   void _onChipSelected(int? index) {
     setState(() {
       _selectedChoiceIndex = index;
       _selectedRowData = null;
-      _showPigPenPenNumber = false; // Reset to show PigPen when switching chips
+      _currentView = _buildInitialView();
     });
   }
 
@@ -39,28 +215,22 @@ class _RecordsState extends State<Records> {
     setState(() {
       _selectedChoiceIndex = null;
       _selectedRowData = rowData;
+      _currentView = QRCodeStatus(pigData: rowData);
     });
   }
 
-  void _showPigPenPenNumberScreen(Map<String, String> rowData) async {
-    final result = await Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => PigPenPenNumber(
-          number: rowData['number']!,
-          type: rowData['type']!,
-          pigCount: rowData['pig count']!,
-          maxPigs: rowData['max pigs']!,
-          pigNumbers: []
-        ),
-      ),
-    );
-
-    if (result == 3) {
-      setState(() {
-        _selectedChoiceIndex = 3; // Set to PigPen chip
-      });
-    }
+  void _showPigPenPenNumberScreen(Map<String, String> rowData) {
+    setState(() {
+      _selectedChoiceIndex = 3;
+      _selectedRowData = rowData;
+      _currentView = PigPenPenNumber(
+        number: rowData['number']!,
+        type: rowData['type']!,
+        pigCount: rowData['pig count']!,
+        maxPigs: rowData['max pigs']!,
+        pigNumbers: [], // Fetch and pass actual pig numbers if needed
+      );
+    });
   }
 
   @override
@@ -92,40 +262,15 @@ class _RecordsState extends State<Records> {
                         ),
                       ),
                     ),
-                    SizedBox(
-                      height: 6.h,
-                    ),
+                    SizedBox(height: 6.h),
                     Container(
                       padding: EdgeInsets.symmetric(horizontal: 15.w),
                       child: Chips(onChipSelected: _onChipSelected),
                     ),
-                    SizedBox(
-                      height: 10.h,
-                    ),
+                    SizedBox(height: 10.h),
                     Container(
-                      //Container containing Screens
-                      child: Column(
-                        children: [
-                          if (_selectedRowData != null && _showPigPenPenNumber)
-                            PigPenPenNumber(
-                              number: _selectedRowData!['number']!,
-                              type: _selectedRowData!['type']!,
-                              pigCount: _selectedRowData!['pig count']!,
-                              maxPigs: _selectedRowData!['max pigs']!,
-                              pigNumbers: [],
-                            )
-                          else if (_selectedRowData != null)
-                            QRCodeStatus(pigData: _selectedRowData!)
-                          else ...[
-                            if (_selectedChoiceIndex == 0)
-                              PigList(onRowSelected: _showQRCodeStatus),
-                            if (_selectedChoiceIndex == 1) FeedInventory(),
-                            if (_selectedChoiceIndex == 2) MedicalRecords(),
-                            if (_selectedChoiceIndex == 3)
-                              PigPen(onRowSelected: _showPigPenPenNumberScreen),
-                          ],
-                        ],
-                      ),
+                      // Container containing Screens
+                      child: _currentView,
                     ),
                   ],
                 ),
@@ -137,5 +282,3 @@ class _RecordsState extends State<Records> {
     );
   }
 }
-
-
